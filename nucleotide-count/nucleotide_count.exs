40d61c2a36786a683ebf1cf8ed2a81f2a44e71f1
PigNatovsky <pignatovsky@gmail.com>
2017-05-02 17:24:26 +0200

    First commit

---
+++ nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,40 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a NucleotideCount strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+
+  def count([], nucleotide) when nucleotide in @nucleotides do
+    0
+  end
+
+  def count(strand, nucleotide) when nucleotide in @nucleotides do
+    Enum.reduce(strand, 0, fn(elem, acc) -> if elem == nucleotide do acc + 1 else acc end end)
+  end
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    Enum.reduce strand, %{?A => 0, ?C => 0, ?G => 0, ?T => 0}, fn(letter, acc) ->
+      Map.update(acc, letter, 1, &(&1 + 1))
+    end
+  end
+end
